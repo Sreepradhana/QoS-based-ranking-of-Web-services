cfff9e0732531dcdd9c2446fca67244712fb34e6
sreepradhana <pradhu.sree@gmail.com>
2016-01-18 17:00:12 -0500

    updating files

--- PegSolitaire/PegSolitaire/test.py
+++
@@ -1,9 +0,0 @@
-import argparse
-
-parser = argparse.ArgumentParser(description='Process some integers.')
-parser.add_argument("--input", type=str)
-parser.add_argument("--flag", type = int)
-	
-
-args = parser.parse_args()
-print(args.accumulate(args.integers))
