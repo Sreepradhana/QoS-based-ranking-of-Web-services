28a9151f6fff7d8fc046be5f3fae21283a31e692
sreepradhana <pradhu.sree@gmail.com>
2016-01-18 15:02:18 -0500

    AIAssign1

---
+++ PegSolitaire/search.py
@@ -0,0 +1,28 @@
+import pegSolitaireUtils
+import config
+from sys import maxint
+#def ItrDeepSearch(pegSolitaireObject):
+for x in range(0 to 5)
+        print "Hi"
+        
+	#################################################
+	# Must use functions:
+	# getNextState(self,oldPos, direction)
+	# 
+	# we are using this function to count,
+	# number of nodes expanded, If you'll not
+	# use this grading will automatically turned to 0
+	#################################################
+	#
+	# using other utility functions from pegSolitaireUtility.py
+	# is not necessary but they can reduce your work if you 
+	# use them.
+	# In this function you'll start from initial gameState
+	# and will keep searching and expanding tree until you 
+	# reach goal using Iterative Deepning Search.
+	# you must save the trace of the execution in pegSolitaireObject.trace
+	# SEE example in the PDF to see what to save
+	#
+	#################################################
+#	return True
+
